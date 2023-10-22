e2ef30e04bbb83b8794038d00b825921f023b44c
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-10-21 21:51:31 -0700

    Debounced input fetch calls

--- src/lib/LocField.svelte
+++ src/lib/LocField.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-	export let value: string = '';
+  import { createEventDispatcher } from "svelte";
+  import { debounceInput } from "./debounce";
+
+  export let value: string = "";
+
+  const dispatch = createEventDispatcher();
+
+  const handleInput = debounceInput((e) => {
+    const inputValue = (e.target as HTMLInputElement).value;
+    dispatch("inputChange", { value: inputValue });
+  }, 500);
 </script>
 
 <form on:submit|preventDefault>
-	<input
-		name="search"
-		type="search"
-		placeholder="Search for location..."
-		class="shrink"
-		bind:value
-	/>
+  <input
+    name="search"
+    type="search"
+    placeholder="Search for location..."
+    class="shrink"
+    on:input={handleInput}
+  />
 </form>
